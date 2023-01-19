893311a6bd8db93824a288a7fcadf697d687801b
River <riverehill@gmail.com>
2023-01-18 22:48:13 -0800

    Setup views

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
 const dotenv = require('dotenv')
 
 dotenv.config({ path: './config/config.env' })
 
 const app = express()
+
+// ejs
+app.use(expressLayouts)
+app.set('view engine', 'ejs')
+
+// routes
+app.use('/', require('./routes/index'))
+app.use('/users', require('./routes/users'))
 
 const PORT = process.env.port || 3000
 
